59b6d1517792be0e19e99a548c9308e8733bfdf9
Kyle Thompson <kthompson@713@gmail.com>
2020-02-28 08:20:19 -0600

    side nav button styling

--- src/hcss-navigation/components/menu-items/side-nav-menu-item.tsx
+++ src/hcss-navigation/components/menu-items/side-nav-menu-item.tsx
@@ -7,9 +7,35 @@ interface SideNavigationMenuItemProps {
 export const SideNavigationMenuItem = ({
   children
 }: SideNavigationMenuItemProps) => {
-  return <ItemWrapper>{children}</ItemWrapper>;
+  return (
+    <ItemWrapper>
+      <Button>{children}</Button>
+    </ItemWrapper>
+  );
 };
 
 const ItemWrapper = styled.div`
+  width: 100%;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding: 4px;
+`;
+
+const Button = styled.button`
+  align-items: center;
+  color: rgb(255, 255, 255);
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  line-height: 1;
+  position: relative;
+  height: 32px;
+  width: 32px;
+  background-color: rgb(7, 71, 166);
+  border-width: 0px;
+  border-radius: 50%;
+  outline: none;
+  padding: 0px;
 `;
